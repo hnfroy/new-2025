04f2702f99040e663daa0c7eaf912c006d54e0f5
Hanif Royyan <hanifroyyan@Hanifs-MacBook-Pro.local>
2025-05-19 10:02:23 +0700

    spelling check

--- src/components/ProjectParallax.tsx
+++ src/components/ProjectParallax.tsx
@@ -106,7 +106,7 @@ export default function ProjectParallax() {
     },
     {
       id: 2,
-      title: "Pengaduan WebApp",
+      title: "Pengaduan Penyidikan WebApp",
       description: "Deskripsi project dua yang tak kalah keren.",
       image:
         "https://images.unsplash.com/photo-1607706189992-eae578626c86?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
